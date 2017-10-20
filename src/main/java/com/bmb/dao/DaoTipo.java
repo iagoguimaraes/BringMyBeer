93a2b096fe06402a0fbe07134e4ad0f0cb90f92c
luis.hlsousa <luis.hlsousa@CAS555088W0720.senacsp.edu.br>
2017-10-20 21:18:55 -0200

    querys DAO

--- src/main/java/com/bmb/dao/DaoTipo.java
+++ src/main/java/com/bmb/dao/DaoTipo.java
@@ -6,6 +6,8 @@
 package com.bmb.dao;
 
 import com.bmb.model.Tipo;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,12 +18,16 @@ import java.util.List;
  */
 public class DaoTipo {
     
-    static List<Tipo> tipos = new ArrayList<>();
-    
-    
     public static void cadastrarTipo(Tipo tipo){
         try{
-            tipos.add(tipo);
+            Connection conn = SQLConnection.getConexao();
+            String sql = "INSERT INTO tipo_produto(tipo) VALUES (?)";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, tipo.getTipo());
+            stmt.execute();
+            stmt.close();
+            conn.close();
+            
         }catch(Exception e){
             System.out.println("Não foi possível cadastrar o tipo");
         }
@@ -29,53 +35,43 @@ public class DaoTipo {
     
     public static void alterarTipo(Tipo tipo){
     	try{
-    		for(int i = 0; i < tipos.size(); i++){
-    			if(tipos.get(i).getIdTipo() == (tipo.getIdTipo())){
-    				tipos.add(i, tipo);
-    				break;
-    			}
-    		}
+            Connection conn = SQLConnection.getConexao();
+            String sql = "Inserir query";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+
     	}catch(Exception e){
     		System.out.println("Não foi possível alterar o tipo");
     	}
     }
     
-    public static Tipo obter(int idTipo){
-    	Tipo tipo = new Tipo();
-
-    	try{
-    		for(int i = 0; i < tipos.size(); i++){
-    			if(tipos.get(i).getIdTipo() == idTipo){
-    				tipo = tipos.get(i);
-    				break;
-    			} 
-    		}
-    	} catch(Exception e){
-    		System.out.println("Não foi possível obter o tipo");
-    	}
-    	return tipo;
-    }
-    
-    public static List<Tipo> obter(){
-    	List<Tipo> tiposCadastrados = new ArrayList<>();
-    	try{
-    		tiposCadastrados = tipos;
-    	}catch(Exception e){
-    		System.out.println("Não foi possível obter os tipos");
-    	}
-    	return tiposCadastrados;
-    }
-    
-    public static void deletarTipo(int idTipo){
-    	try{
-    		for(int i = 0; i < tipos.size(); i++){
-    			if(tipos.get(i).getIdTipo() == idTipo){
-    				tipos.remove(i);
-    				break;
-    			} 
-    		}
-    	} catch(Exception e){
-    		System.out.println("Não foi possível remover o tipo");
-    	}
-    }
+//    public static Tipo obter(int idTipo){
+//    	try{
+//        
+//    	} catch(Exception e){
+//    		System.out.println("Não foi possível obter o tipo");
+//    	}
+//    }
+//    
+//    public static List<Tipo> obter(){
+//    	List<Tipo> tiposCadastrados = new ArrayList<>();
+//    	try{
+//    		tiposCadastrados = tipos;
+//    	}catch(Exception e){
+//    		System.out.println("Não foi possível obter os tipos");
+//    	}
+//    	return tiposCadastrados;
+//    }
+//    
+//    public static void deletarTipo(int idTipo){
+//    	try{
+//    		for(int i = 0; i < tipos.size(); i++){
+//    			if(tipos.get(i).getIdTipo() == idTipo){
+//    				tipos.remove(i);
+//    				break;
+//    			} 
+//    		}
+//    	} catch(Exception e){
+//    		System.out.println("Não foi possível remover o tipo");
+//    	}
+//    }
 }
